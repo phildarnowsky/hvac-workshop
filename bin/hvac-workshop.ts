b280adb98316f45000d15a2adb0a94ba2694b738
Phil Darnowsky <phil@darnowsky.com>
2022-01-29 23:40:52 -0500

    Ignore last thing eslint complained about

--- bin/hvac-workshop.ts
+++ bin/hvac-workshop.ts
@@ -3,4 +3,6 @@ import * as cdk from '@aws-cdk/core'
 import { BuildingApiStack } from '../lib/building-api-stack'
 
 const app = new cdk.App()
+
+// eslint-disable-next-line no-new
 new BuildingApiStack(app, 'HvacWorkshopStack')
